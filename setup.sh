03b5d6b588d5f567de52f45c33daedb2b50f7f8e
TurtleRuss <tttturtleruss@gmail.com>
2024-07-23 18:10:10 +0800

    fix: shellcheck error

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 # Install requirements
 pip install docker gitpython
 
-for d in $(ls); do
+for d in ./*; do
     if [ ! -d "$d" ]; then
         continue
     fi;
-    if [ -f ${d}/build.py ]; then
+    if [ -f "${d}"/build.py ]; then
         echo "Find no build.py for ${d}"
         continue
     fi;
@@ -19,6 +19,6 @@ for d in $(ls); do
     # Build docker image
     if grep -q build .stages; then
         echo "Building ${d}"
-        python setup.py --mode=build --path=/tmp/${d}
+        python setup.py --mode=build --path=/tmp/"${d}"
     fi;
 done;
