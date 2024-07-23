4dbf291e12e89c70842cc7cc66a9188f1f5fb5a9
TurtleRuss <tttturtleruss@gmail.com>
2024-07-23 16:16:16 +0800

    add neqo

---
+++ neqo/generate.py
@@ -0,0 +1,56 @@
+from rich import print
+import subprocess
+
+def list_all_tags_for_remote_git_repo(url):
+    """
+    Given a repository URL, list all tags for that repository
+    without cloning it.
+
+    This function use "git ls-remote", so the
+    "git" command line program must be available.
+    """
+    # Run the 'git' command to fetch and list remote tags
+    result = subprocess.run([
+        "git", "ls-remote", "--tags", repo_url
+    ], stdout=subprocess.PIPE, text=True)
+
+    # Process the output to extract tag names
+    output_lines = result.stdout.splitlines()
+    tags = [
+        line.split("refs/tags/")[-1] for line in output_lines
+        if "refs/tags/" in line and "^{}" not in line
+    ]
+
+    return tags
+
+repo_url = "https://github.com/mozilla/neqo.git"
+
+template = """
+FROM ubuntu:20.04
+
+# RUN sed -i.bak 's|https\?://archive.ubuntu.com|https://mirrors.aliyun.com|g' /etc/apt/sources.list
+RUN apt update && apt upgrade -y
+RUN apt install -y git python3-dev curl build-essential mercurial python-dev ninja-build gyp libclang-dev
+RUN curl https://sh.rustup.rs -sSf | sh -s -- -y 
+ENV PATH=$PATH:/root/.cargo/bin/
+RUN git clone https://github.com/mozilla/neqo.git
+WORKDIR /neqo
+RUN git checkout {tag}
+RUN git clone https://github.com/nss-dev/nss.git
+RUN hg clone https://hg.mozilla.org/projects/nspr
+WORKDIR /neqo/nss
+ENV NSS_DIR=/neqo/nss
+RUN ./build.sh
+WORKDIR /neqo
+ENV LD_LIBRARY_PATH=/neqo/dist/Debug/lib/
+RUN cargo build
+
+EXPOSE 12345
+CMD [ "/neqo/target/debug/neqo-server", "[::]:12345" ]
+"""
+tags = list_all_tags_for_remote_git_repo(repo_url)
+for tag in tags:
+    with open(f"./Dockerfile.{tag}", "w") as f:
+        f.write(template.format(tag=tag))
+
+print(f"Generate [green]{len(tags)}[/green] Dockerfile for mozilla/neqo")
