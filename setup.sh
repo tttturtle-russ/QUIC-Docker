20d2b808e8f499cde26e3588bbf944cd4872bc7e
TurtleRuss <tttturtleruss@gmail.com>
2024-07-23 16:57:20 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+# Install requirements
+pip install docker gitpython
+
+for d in $(ls); do
+    if [ ! -d "$d" ]; then
+        continue
+    fi;
+    if [ -f ${d}/build.py ]; then
+        echo "Find no build.py for ${d}"
+        continue
+    fi;
+    # Generate Dockerfile if needed
+    if grep -q generate .stages; then
+        echo "Generating ${d}"
+        python setup.py --mode=generate
+    fi;
+    # Build docker image
+    if grep -q build .stages; then
+        echo "Building ${d}"
+        python setup.py --mode=build --path=/tmp/${d}
+    fi;
+done;
